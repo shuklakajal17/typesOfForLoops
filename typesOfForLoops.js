cca1fb22fcb69033d8c3a3200ce1790fbd8bcc0b
kajal shukla <shuklakajal1703@gmail.com>
2025-06-08 13:35:04 +0530

    Add files via upload

---
+++ typesOfForLoops.js
@@ -0,0 +1,32 @@
+// TYPES OF FOR LOOPS
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//for let
+for(let i = 0; i <arr.length; i++){
+    console.log(arr[i]);
+}
+
+
+//for of
+for(let value of arr){
+    console.log(value);
+}
+
+
+//for in 
+let person = {
+    id : 1,
+    name : "raju",
+    age : 19
+}
+
+
+//to get keys
+for (let key in person){
+    console.log(key)
+}
+
+//to get values
+for (let key in person) {
+    console.log(person[key]);
+}
